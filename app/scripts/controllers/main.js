300e86a0204312810250113ddee668eb3e5df1df
MrElbow <info@mrelbow.com>
2016-07-01 04:37:31 -0600

    First code commit
    Base code after generating the project with 'yo angular'

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name autoTwitterApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the autoTwitterApp
+ */
+angular.module('autoTwitterApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
