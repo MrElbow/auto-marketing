7012ee6b77087b030622bf72a83a3a0a0c33228c
MrElbow <info@mrelbow.com>
2016-07-02 08:39:37 -0600

    Add backend with jersey
    This version generate a standalone jar

--- backend/simple-service/src/main/java/com/underdog/jersey/grizzly/MyResource.java
+++ backend/simple-service/src/main/java/com/underdog/jersey/grizzly/MyResource.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.underdog.jersey.grizzly;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
--- backend/simple-service/src/test/java/com/underdog/jersey/grizzly/MyResourceTest.java
+++ backend/simple-service/src/test/java/com/underdog/jersey/grizzly/MyResourceTest.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.underdog.jersey.grizzly;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
