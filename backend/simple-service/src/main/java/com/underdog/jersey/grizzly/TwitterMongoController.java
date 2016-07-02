bb8136911051464bdbb5072e2a886a5c208c4feb
MrElbow <info@mrelbow.com>
2016-07-02 15:36:49 -0600

    Add the TwitterMongoController class
    This class is to handle the communication with Mongo and data
    manipulation

--- backend/simple-service/src/main/java/com/underdog/jersey/grizzly/TwitterMongoController.java
+++ backend/simple-service/src/main/java/com/underdog/jersey/grizzly/TwitterMongoController.java
@@ -1,11 +1,30 @@
 package com.underdog.jersey.grizzly;
 
+import org.bson.Document;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+import static java.util.Arrays.asList;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
 
 /**
  * Root resource (exposed at "myresource" path)
  */
-public class MongoController {
+public class TwitterMongoController {
 
+	MongoClient mongoClient;
+	MongoDatabase db;
+	
+	public TwitterMongoController(){
+		mongoClient = new MongoClient();
+		db = mongoClient.getDatabase("twitter");
+	}
+	
     /**
      * Method handling HTTP GET requests. The returned object will be sent
      * to the client as "text/plain" media type.
@@ -16,6 +35,11 @@ public class MongoController {
     
     
     public String autoFavorite(String queryString) {
+    	/*DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ENGLISH);
+    	db.getCollection("favoritedUsers").insertOne(
+    	        new Document("userId","32103983120812038102381023802138")
+    	                .append("user", "testUser")
+    	                .append("date", format.parse("2014-10-01T00:00:00Z")));*/
     	/*
     	// The factory instance is re-useable and thread safe.
         Twitter twitter = TwitterFactory.getSingleton();
